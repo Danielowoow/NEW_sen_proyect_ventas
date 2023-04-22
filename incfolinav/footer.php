142ac4b73e3285aeb61cb2b30f1ddbd505a6f402
Carlos Daniel Quispe Quispe <quispecarlosdaniel078@gmail.com>
2023-04-21 19:15:55 -0500

    JIIJA

---
+++ incfolinav/footer.php
@@ -0,0 +1,160 @@
+<footer>
+  <div>
+    <div>
+      <h5>Suscríbete a nuestras promociones</h5>
+      <p>Ingresa tu correo electrónico para recibir las mejores ofertas</p>
+      <form action="subscribe.php" method="post">
+        <div>
+          <input type="email" placeholder="Correo electrónico" required>
+          <button type="submit">Suscribirse</button>
+        </div>
+      </form>
+    </div>
+    <div>
+      <h5>Contáctanos</h5>
+      <p>Chatea con nosotros<br>
+      Te atendemos las 24hrs</p>
+      <p>Escríbenos:<br>
+      Estamos para ayudarte<br>
+      QosqoMarket9989@jijija.pe</p>
+      <p>Llámanos<br>
+      Venta Telefónica 931998025<br>
+      De lunes a domingo de 8:00am a 8:00pm</p>
+    </div>
+    <div>
+      <div>
+        <h5>Secciones</h5>
+        <div>
+          <button type="button">Nosotros</button>
+          <button type="button">Extra Garantía</button>
+          <button type="button">Nuestros servicios</button>
+          <button type="button">Venta empresa</button>
+          <button type="button">Trabaja con nosotros</button>
+        </div>
+      </div>
+      <div>
+        <h5>Atención al cliente</h5>
+        <div>
+          <p>Atención al cliente</p>
+          <ul>
+            <li><a href='#'>Preguntas frecuentes</a></li>
+            <li><a href='#'>Cambios y devoluciones</a></li>
+            <li><a href='#'>Términos y condiciones de compra</a></li>
+            <li><a href='#'>Politica de privacidad</a></li>
+            <li><a href='#'>Contáctanos</a></li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h5>Otros</h5>
+        <ul>
+          <li>Síguenos en
+          <ul>
+            <li><a href="https://www.senati.edu.pe">Facebook</a></li>
+            <li><a href="https://www.senati.edu.pe">Twitter</a></li>
+            <li><a href="https://www.senati.edu.pe">Instagram</a></li>
+            <li><a href="https://www.senati.edu.pe">Youtube</a></li>
+          </ul>
+          </li>
+          <li><a href='#'>Libro de reclamaciones</a></li>
+        </ul>
+        <h5>Métodos de pago</h5>
+        <ul>
+          <li>MASTERCARD</li>
+          <li>VISA</li>
+          <li>DINERS</li>
+          <li>AMEX</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div>
+    <p>© 2023 - Qosqo Market. Todos los derechos reservados.</p>
+  </div>
+  <style>
+    body {
+    background: rgb(34,193,195);
+    background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(190,21,50,1) 100%);
+    text-align: left;
+  }
+.btn-neon{
+    position: relative;
+    display: inline-block;
+    padding: 6px 12px;
+    color: #da1c1c;
+    letter-spacing: 4px;
+    text-decoration: none;
+    font-size: 8px;
+    overflow: hidden;
+    transition: 0.2s;
+    background: rgb(131,58,180);
+    background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(87,82,80,1) 0%, rgba(252,225,69,1) 0%, rgba(253,66,29,1) 100%);
+}
+
+.btn-neon:hover{
+    background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(190,21,50,1) 100%);
+    box-shadow: 0 0 10px rgba(190,21,50,1), 0 0 40px rgba(190,21,50,1), 0 0 80px rgba(190,21,50,1);
+    transition-delay: 1s;
+}
+
+.btn-neon span{
+    position: absolute;
+    display: block;
+}
+
+#span1{
+    top: 0;
+    left: 100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(90deg, transparent,#a945c7);
+}
+
+.btn-neon:hover #span1{
+    left: 100%;
+    transition: 1s;
+}
+
+#span3{
+    bottom: 0;
+    right: 100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(270deg, transparent,#a945c7);
+}
+
+.btn-neon:hover #span3{
+    right: 100%;
+    transition: 1s;
+    transition-delay: 0.5s;
+}
+
+#span2{
+    top: -100%;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(180deg,transparent,#a945c7);
+}
+
+.btn-neon:hover #span2{
+    top: 100%;
+    transition: 1s;
+    transition-delay: 0.25s;
+}
+
+#span4{
+    bottom: -100%;
+    left: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(360deg,transparent,#a945c7);
+}
+
+.btn-neon:hover #span4{
+    bottom: 100%;
+    transition: 1s;
+    transition-delay: 0.75s;
+}
+  </style>
+</footer>
---
+++ loginregis.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>INICIAR</title>
+    <?php include 'incfolinav/link.php'; ?>
+    <link rel="stylesheet" type="text/css" href="css/loginstyle.css" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+  
+  </head>
+  <body>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="usuarios/login.php" metod="post"class="sign-in-form">
+            <h2 class="title">INICIAR SESIÓN</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="email" name="email" placeholder="Correo" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" name="password" placeholder="Contraseña" />
+            </div>
+                <a href="https://wa.me/51931998025?text=Olvidé%20mi%20contraseña.">¿Te olvidaste tu contraseña?</a>
+            <input type="submit" value="INICIAR" class="btn solid" />
+          </form>
+
+
+          <form action="usuarios/registro.php" metod="post" class="sign-up-form">
+            <h2 class="title">REGISTRATE</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="DNI" required/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Nombre" required/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Correo" required/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" name="password" placeholder="Contraseña" pattern=".{8,}" title="La contraseña debe contener al menos 8 caracteres" required />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" name="confirm_password" placeholder="Confirmar contraseña" pattern=".{8,}" title="Las contraseñas deben coincidir" required />
+            </div>
+            <div>
+            <input type="checkbox" name="terms" id="terms" required>
+            <label for="terms">Acepto los <a href="https://wa.me/51931998025?text=TERMINOS%20Y%20CONDICIONES" target="_blank">términos y condiciones</a></label>
+          </div>
+            <input type="submit" value="REGISTRAR" class="btn solid" />
+          </form>
+        </div>
+      </div>
+      <div class="panels-container">
+
+        <div class="panel left-panel">
+            <div class="content">
+                <h3>¿No tienes una cuenta?</h3>
+                <p>Dale click, y create una.</p>
+                <button class="btn transparent" id="sign-up-btn">CREAR</button>
+            </div>
+            <img src="./img/log.svg" class="image" alt="">
+        </div>
+
+        <div class="panel right-panel">
+            <div class="content">
+                <h3>¿Quieres iniciar sesión?</h3>
+                <p>INICIAR SESIÓN</p>
+                <button class="btn transparent" id="sign-in-btn">INICIAR</button>
+                
+            </div>
+            <img src="./img/register.svg" class="image" alt="">
+        </div>
+      </div>
+    </div>
+    <?php //include 'incfolinav/footer.php'?>
+    <script src="js/loginstyle.js"></script>
+  </body>
+</html>
